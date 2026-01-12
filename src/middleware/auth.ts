c069d4dfa52377b02632e6afb73b177c03c917d0
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-01-12 22:33:50 +0600

    auth continoue

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { Role } from "../generated/prisma/enums";
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken"
+
+declare global{
+    namespace Express{
+        interface Request{
+            user:JwtPayload
+        }
+    }
+}
 
 const auth = (roles?: Role) => {
     return (req: Request, res: Response, next: NextFunction) => {
@@ -9,6 +17,8 @@ const auth = (roles?: Role) => {
         if (!token) res.send("Please Provide token")
         try {
             const decode = jwt.verify(token as string, 'very secret')
+            // console.log(decode)
+            req.user=decode as JwtPayload //use decode info globally. whole application
             if(!decode) return res.send("Unauthorized")
             next()
         }
--- src/modules/usage-log/log.route.ts
+++ src/modules/usage-log/log.route.ts
@@ -2,11 +2,12 @@
 
 import { Router } from "express";
 import { logController } from "./log.controller";
+import auth from "../../middleware/auth";
 
 
 const logRoute = Router()
 
-logRoute.post('/',logController.createUsageLog)
+logRoute.post('/',auth(),logController.createUsageLog)
 logRoute.get('/',logController.getUsageLog)
 
 export default logRoute
