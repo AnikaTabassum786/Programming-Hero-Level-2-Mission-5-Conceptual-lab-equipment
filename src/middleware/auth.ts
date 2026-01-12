5e26a756e0c001bc65fc5dadd5007800833a0b7c
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-01-12 21:43:00 +0600

    login done

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,19 +1,21 @@
-import { NextFunction,Request,Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import { Role } from "../generated/prisma/enums";
+import jwt from "jsonwebtoken"
 
+const auth = (roles?: Role) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const token = req.headers.authorization?.split(" ")[1];
 
-const auth =(roles?:Role)=>{
-  return(req:Request,res:Response,next:NextFunction)=>{
-    const token = req.headers.authorization?.split(" ")[1];
-
-    if(!token) res.send("Please Provide token")
-         try{
-    
-  }
-  catch(error){
-    console.log(error)
-  }
-  }
-
- 
+        if (!token) res.send("Please Provide token")
+        try {
+            const decode = jwt.verify(token as string, 'very secret')
+            if(!decode) return res.send("Unauthorized")
+            next()
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
 }
+
+export default auth;
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,18 +1,41 @@
 import { RequestHandler } from "express";
 import { prisma } from "../../lib/prisma";
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken'
 
-const register: RequestHandler= async(req,res)=>{
+const register: RequestHandler = async (req, res) => {
     const payload = req.body;
 
+    const hashPassword = await bcrypt.hash(payload.password, 10)
+
     const user = await prisma.user.create({
-        data: payload,
+        data: { ...payload, password: hashPassword },
     })
 
     res.send({
-        message:"Register successfully", data: user
+        message: "Register successfully", data: user
     })
 }
 
-export const userController ={
-        register
-    }
+const login: RequestHandler = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({ where: {email}, })
+
+    if (!user) return res.send({ message: "User not found" })
+
+    const matchPass = await bcrypt.compare(password, user.password)
+    if (!matchPass) return res.send({ message: "Invalid Password" })
+
+    const token =  jwt.sign(
+        { id: user.id, role: user.role }, "very secret",
+        { expiresIn: "7d" }
+    );
+    res.send({ message: "Login successfully", token })
+
+}
+
+export const userController = {
+    register,
+    login
+}
