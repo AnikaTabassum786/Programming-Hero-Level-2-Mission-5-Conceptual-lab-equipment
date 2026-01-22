6d8cc304dfdea41130a972662451e795a78fd751
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-01-22 22:20:48 +0600

    better auth

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,17 @@ import express from "express"
 import cors from "cors"
 
 import routes from "./routes"
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express()
+app.use(cors({origin: process.env.FRONTEND_URL,credentials: true}))
+
+app.all("/api/auth/*splat", toNodeHandler(auth)); 
+
 
 app.use(express.json())
-app.use(cors())
+
 
 
 app.use('/api/vi',routes)
