5341246d35a3f2fb8931e8bfe8d56faa66a5ce16
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-01-31 22:41:13 +0600

    2FA

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,12 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import { twoFactor } from "better-auth/plugins"
 
 export const auth = betterAuth({
+
+   appName: "lab log",
+
   database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
@@ -14,4 +18,14 @@ export const auth = betterAuth({
     enabled: true, 
     minPasswordLength:6,
   },
+  socialProviders: {
+        github: { 
+            clientId: process.env.GITHUB_CLIENT_ID as string, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+            redirectURI: `${process.env.FRONTEND_URL}/api/auth/callback/github`
+        }, 
+    },
+     plugins: [
+        twoFactor() 
+    ]
 });
